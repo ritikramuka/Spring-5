2fcedb5d2eae8324a15903ef15331b5ea8da3654
Ritik Ramuka <ritik.ramuka@gmail.com>
2021-01-06 05:07:12 +0530

    update

---
+++ src/main/java/com/Ramuka/spring5webApp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package com.Ramuka.spring5webApp.controllers;
+
+import com.Ramuka.spring5webApp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors",authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
